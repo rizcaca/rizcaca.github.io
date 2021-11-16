220f538738c24f9f6bd2cd6d5f93454d97f672fe
root <root@DESKTOP-KOL6PKU.localdomain>
2021-11-16 08:19:41 +0700

    initial commit

--- js/custom.js
+++ js/custom.js
@@ -9,21 +9,21 @@ $("#penerima").html(to);
 console.log(to);
 
 
-window.addEventListener('load', function () {
-    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    var source = audioCtx.createBufferSource();
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'audio/audio.mp3');
-    xhr.responseType = 'arraybuffer';
-    xhr.addEventListener('load', function (r) {
-        audioCtx.decodeAudioData(
-                xhr.response, 
-                function (buffer) {
-                    source.buffer = buffer;
-                    source.connect(audioCtx.destination);
-                    source.loop = false;
-                });
-        source.start(0);
-    });
-    xhr.send();
-});
+// window.addEventListener('load', function () {
+//     var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+//     var source = audioCtx.createBufferSource();
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'audio/audio.mp3');
+//     xhr.responseType = 'arraybuffer';
+//     xhr.addEventListener('load', function (r) {
+//         audioCtx.decodeAudioData(
+//                 xhr.response, 
+//                 function (buffer) {
+//                     source.buffer = buffer;
+//                     source.connect(audioCtx.destination);
+//                     source.loop = false;
+//                 });
+//         source.start(0);
+//     });
+//     xhr.send();
+// });
